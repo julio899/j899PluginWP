cbc8a74a47f5586ff7aced90de93e820826466a7
Julio Vinachi <julio899@gmail.com>
2019-08-03 20:33:30 -0400

    loader

--- includes/views/settings.php
+++ includes/views/settings.php
@@ -6,17 +6,17 @@
 	<div id="message" class="updated woocommerce-message wc-connect">
 		<p><strong>Bienvenido a J899 Plugin</strong> – Ya estas casi listo para completar :)</p>
 		<input type="text" value="<?php echo J899_LIC;?>" class="location-input" size="40">
-		<p class="submit"><a href="http://localhost/wpTest/wp-admin/admin.php?page=wc-setup" class="button-primary">Validar Licencia</a> <a class="button-secondary skip" href="<?php echo esc_url( admin_url( 'admin.php?page=wc-addons' ) ); ?>">Obtener Nueva</a></p>
+		<p class="submit"><a href="http://localhost/wpTest/wp-admin/admin.php?page=wc-setup" class="button-primary" id="btn-validar-lic">Validar Licencia</a> <a class="button-secondary skip" href="<?php echo esc_url( admin_url( 'admin.php?page=wc-addons' ) ); ?>">Obtener Nueva</a></p>
 	</div>
-	<?php echo J899_DIR; ?>
-	<?php echo esc_url( plugins_url( '../img/loader.gif', __FILE__ ) ); ?>
+	
 	<div id="ajax-response"></div>
 	<br class="clear">
 </div>
 
-<div class="j899_loader">
+<div id="j899-loader" class="j899_loader no_display">
 	<div class="loader-internal"></div>
 </div>
+<script src="<?php echo esc_url( plugins_url( '../js/start.js', __FILE__ ) ); ?>"></script>
 
 <style>
 	.j899_loader{
@@ -28,6 +28,7 @@
 	    right: 0;
 	    z-index: 10000;
 	}
+/*
 	.loader-internal{
 		position: fixed;
 	    left: 0px;
@@ -37,4 +38,33 @@
 	    z-index: 9999;
 	    background: url('<?php echo esc_url( plugins_url( '../img/loader.gif', __FILE__ ) ); ?>') 50% 50% no-repeat rgba(249, 249, 249, 0);
 	}
+*/
+
+.loader-internal{
+  border: 7px solid #d4d4d4;
+  border-top: 7px solid #3498db;
+  border-bottom: 7px solid #3498db;
+  border-radius: 50%;
+  width: 70px;
+  height: 70px;
+  animation: spin 1.5s linear infinite;
+  position: absolute; /* cambiamos de fixed a absolute */
+  /* Ponemos el valor de left, bottom, right y top en 0 */
+  left: 0;
+  bottom: 0; 
+  right: 0; 
+  top: 0; 
+  margin: auto; /* Centramos vertical y horizontalmente */
+}
+
+@keyframes spin {
+    0% { transform: rotate(0deg); }
+    100% { transform: rotate(360deg); }
+}
+
+
+.no_display{
+	display: none;
+
+}
 </style>
