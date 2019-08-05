290a0a55ac906be055caf3481e01fb89b3cb4169
Julio Vinachi <julio899@gmail.com>
2019-08-04 23:47:55 -0400

    sincronizando con el servidor

--- includes/js/start.js
+++ includes/js/start.js
@@ -1,8 +1,29 @@
 var loader = document.getElementById('j899-loader');
-document.getElementById('btn-validar-lic').addEventListener('click',(e)=>{
+document.getElementById('btn-validar-lic').addEventListener('click', e => {
 	e.preventDefault();
 	loader.classList.remove('no_display');
-	setTimeout(()=>{
-		loader.classList.add('no_display');	
-	},3000)
+	// ---------- POST con CORS configurado en backend --------------
+	var datos = {
+		lic: document.getElementById('lic-code').value
+	};
+	fetch('http://localhost:8000/lic', {
+		method: 'POST',
+		body: JSON.stringify(datos),
+		headers: {
+			'content-type': 'application/json'
+		}
+	}).then(r => {
+		r.json().then(data => {
+			if (data.error !== null) {
+				console.log(data);
+			} else {
+				console.info(data.data.status);
+				loader.classList.add('no_display');
+				document.getElementById('message-confirm').classList.remove('no_display');
+				document.getElementById('message').classList.add('no_display');
+				document.getElementById('lic-confirmation').innerText=datos.lic;
+			}
+		});
+	});
+	// ---------- // POST con CORS configurado en backend ----------
 });
--- includes/views/settings.php
+++ includes/views/settings.php
@@ -3,10 +3,19 @@
 		<h1 class="wp-heading-inline">Configuracion de Liciencia Plugin J899</h1>
 		<a href="<?php echo esc_url( admin_url( 'admin.php?page=j899-woocommerce' ) ); ?>" class="page-title-action">Ir a Woocomerce</a>
 	</div>
+
 	<div id="message" class="updated woocommerce-message wc-connect">
 		<p><strong>Bienvenido a J899 Plugin</strong> – Ya estas casi listo para completar :)</p>
-		<input type="text" value="<?php echo J899_LIC;?>" class="location-input" size="40">
+		<input id="lic-code" type="text" value="<?php echo J899_LIC;?>" class="location-input" size="40">
 		<p class="submit"><a href="http://localhost/wpTest/wp-admin/admin.php?page=wc-setup" class="button-primary" id="btn-validar-lic">Validar Licencia</a> <a class="button-secondary skip" href="<?php echo esc_url( admin_url( 'admin.php?page=wc-addons' ) ); ?>">Obtener Nueva</a></p>
+	</div>
+
+	<div id="message-confirm" class="updatedConfirm woocommerce-message wc-connect no_display">
+		<p>
+			<strong>Excelente Plugin Validado y Confirmado</strong> – Ya estas sincronizado
+			<br>
+			<strong>Licencia: </strong> <label id="lic-confirmation"></label>
+		</p>	
 	</div>
 	
 	<div id="ajax-response"></div>
@@ -19,6 +28,14 @@
 <script src="<?php echo esc_url( plugins_url( '../js/start.js', __FILE__ ) ); ?>"></script>
 
 <style>
+	.updatedConfirm{
+		/*border-left-color: #08fb79;*/
+		background-color: rgba(30, 213, 45, 0.25);
+		border-left: 4px solid rgb(0, 255, 21);
+		box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
+		margin: 5px 15px 2px;
+		padding: 1px 12px;
+	}
 	.j899_loader{
 		width: 100vw;
 	    height: 100%;
